fcf745dcccef01a9893c08e492ab5cf7030fad5a
lmaoitsrlyme <73641532+lmaoitsrlyme@users.noreply.github.com>
2022-11-24 12:31:28 +0800

    Update server.js

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ const transporter = nodemailer.createTransport({
     port: 465,
     host: "smtp.gmail.com",
     auth: {
-        user: '',
-        pass: '',
+        user: "ryanishanth@gmail.com",
+        pass: "nbktnqsymmxtpeob"
     },
     secure: true,
 });
